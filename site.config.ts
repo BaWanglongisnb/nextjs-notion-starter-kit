e4a99532061605c7c84b5247469e1df7bd0f8a13
BaWanglongisnb <145077864+BaWanglongisnb@users.noreply.github.com>
2024-05-21 15:29:22 +0800

    site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,20 +6,21 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  // notionid 霸王龙
+  rootNotionSpaceId: a48712c145ca45f0bd23cc9031f81a39,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '霸王龙布拉格',
+  domain: 'https://www.notion.so/Tyrannosaurus-rex-a48712c145ca45f0bd23cc9031f81a39',
+  author: 'kyrie',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'BaWanglongisnb',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
